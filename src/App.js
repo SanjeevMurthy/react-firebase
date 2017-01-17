65ed04bae9ff9e84a14c999a179e16ead8774241
yu92 <Sanjeev.Parashiva@in.tesco.com>
2017-01-17 17:01:09 +0530

    Add comming soon message on Homepage

--- src/App.js
+++ src/App.js
@@ -5,30 +5,37 @@ const logoUrl=require('./assets/yoga-logo.png');
 
 import Material from './components/Material-Component';
 import Header from './components/common/Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 //import Footer from './component/common/Footer';
 //import MainApp from './component/Main';
 
 export default class App extends Component {
-  // render() {
-  //   return (
-  //     <div className="App">
-  //       <div className="App-header">
-  //         <img src={logoUrl} className="App-logo" alt="logo" />
-  //         <h2>Welcome to YogaDathri</h2>
-  //       </div>
-  //       <p className="App-intro">
-  //         <Material/>
-  //       </p>
-  //     </div>
-  //   );
-  // }
-
-  render(){
-    return(
-      <div>
-        <Header/>
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logoUrl} className="App-logo" alt="logo" />
+          <h2>Welcome to YogaDathri</h2>
+        </div>
+        <div className="jumbotron">
+          <div className="container">
+          <p>{`"GO FROM HUMAN BEING DOING YOGA TO A HUMAN BEING YOGA - Baron Baptiste"`}</p>
+          <h3 className="App-Message">{`Currently we're working on this website`}</h3>
+          <h3 className="App-Message">We will come soon be there !!</h3>
+          <p></p>
+          <p></p>
+          <p><a className="btn btn-primary btn-lg" href="https://en.wikipedia.org/wiki/Yoga" role="button">More about YOGA</a></p>
+          </div>
+        </div>
       </div>
     );
   }
-}
 
+  // render(){
+  //   return(
+  //     <div>
+  //       <Header/>
+  //     </div>
+  //   );
+  // }
+}
